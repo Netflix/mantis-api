c5118a2b14931e643dab10cd0c4c175f5ef1bf3b
Cody Rioux <crioux@netflix.com>
2020-02-05 12:08:42 -0800

    Options: Now just returns empty response, outbound filter handles headers.

--- src/main/java/io/mantisrx/api/filters/Options.java
+++ src/main/java/io/mantisrx/api/filters/Options.java
@@ -14,11 +14,6 @@ public class Options extends HttpSyncEndpoint {
     @Override
     public HttpResponseMessage apply(HttpRequestMessage request) {
         HttpResponseMessage resp = new HttpResponseMessageImpl(request.getContext(), request, HttpResponseStatus.NO_CONTENT.code());
-        resp.getHeaders().add(HttpHeaderNames.ACCESS_CONTROL_ALLOW_ORIGIN.toString(), "*");
-        resp.getHeaders().add(HttpHeaderNames.ACCESS_CONTROL_ALLOW_HEADERS.toString(), "Origin, X-Requested-With, Accept, Content-Type, Cache-Control");
-        resp.getHeaders().add(HttpHeaderNames.ACCESS_CONTROL_ALLOW_METHODS.toString(),
-                "GET, OPTIONS, PUT, POST, DELETE");
-        StatusCategoryUtils.setStatusCategory(request.getContext(), ZuulStatusCategory.SUCCESS);
         return resp;
     }
 }
