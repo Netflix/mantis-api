884c062bb9df9dda43e7a5677511a71a5343d828
Cody Rioux <crioux@netflix.com>
2020-02-05 14:32:49 -0800

    OutboundHeaders: Now only adds missing headers.

--- src/main/java/io/mantisrx/api/filters/OutboundHeaders.java
+++ src/main/java/io/mantisrx/api/filters/OutboundHeaders.java
@@ -1,13 +1,9 @@
 package io.mantisrx.api.filters;
 
 import com.netflix.zuul.filters.http.HttpOutboundSyncFilter;
-import com.netflix.zuul.message.http.HttpRequestMessage;
 import com.netflix.zuul.message.http.HttpResponseMessage;
-import com.netflix.zuul.message.http.HttpResponseMessageImpl;
-import com.netflix.zuul.stats.status.StatusCategoryUtils;
-import com.netflix.zuul.stats.status.ZuulStatusCategory;
 import io.netty.handler.codec.http.HttpHeaderNames;
-import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.util.AsciiString;
 
 public class OutboundHeaders extends HttpOutboundSyncFilter {
 
@@ -18,12 +14,18 @@ public class OutboundHeaders extends HttpOutboundSyncFilter {
 
     @Override
     public HttpResponseMessage apply(HttpResponseMessage resp) {
-        resp.getHeaders().add(HttpHeaderNames.ACCESS_CONTROL_ALLOW_ORIGIN.toString(), "*");
-        resp.getHeaders().add(HttpHeaderNames.ACCESS_CONTROL_ALLOW_HEADERS.toString(), "Origin, X-Requested-With, Accept, Content-Type, Cache-Control");
-        resp.getHeaders().add(HttpHeaderNames.ACCESS_CONTROL_ALLOW_METHODS.toString(),
+        addHeaderIfMissing(resp, HttpHeaderNames.ACCESS_CONTROL_ALLOW_ORIGIN, "*");
+        addHeaderIfMissing(resp, HttpHeaderNames.ACCESS_CONTROL_ALLOW_HEADERS,
+                "Origin, X-Requested-With, Accept, Content-Type, Cache-Control");
+        addHeaderIfMissing(resp, HttpHeaderNames.ACCESS_CONTROL_ALLOW_METHODS,
                 "GET, OPTIONS, PUT, POST, DELETE");
-        StatusCategoryUtils.setStatusCategory(resp.getContext(), ZuulStatusCategory.SUCCESS);
         return resp;
+    }
+
+    private void addHeaderIfMissing(HttpResponseMessage resp, AsciiString name, String value) {
+        if (resp.getHeaders().get(name.toString()).size() == 0) {
+            resp.getHeaders().add(name.toString(), value);
+        }
     }
 
     @Override
