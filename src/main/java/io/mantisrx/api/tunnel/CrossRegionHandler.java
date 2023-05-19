a1d78ef48e6a44ca22357389800af242ce98dc5f
Calvin Cheung <ccheung@netflix.com>
2023-05-19 09:22:27 -0700

    separate out flush

--- src/main/java/io/mantisrx/api/tunnel/CrossRegionHandler.java
+++ src/main/java/io/mantisrx/api/tunnel/CrossRegionHandler.java
@@ -307,10 +307,11 @@ public class CrossRegionHandler extends SimpleChannelInboundHandler<FullHttpRequ
                         queue.drainTo(items);
                     }
                     for (String data : items) {
-                        ctx.writeAndFlush(Unpooled.copiedBuffer(data, StandardCharsets.UTF_8));
+                        ctx.write(Unpooled.copiedBuffer(data, StandardCharsets.UTF_8));
                         numMessagesCounter.increment();
                         numBytesCounter.increment(data.length());
                     }
+                    ctx.flush();
                 }
             } catch (Exception ex) {
                 log.error("Error writing to channel", ex);
