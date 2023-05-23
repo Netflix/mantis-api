71abd5fb46ba00fd31ee4e11854daf15d93c078f
Calvin Cheung <ccheung@netflix.com>
2023-05-23 13:06:07 -0700

    Flush every time for websocket

--- src/main/java/io/mantisrx/api/push/MantisWebSocketFrameHandler.java
+++ src/main/java/io/mantisrx/api/push/MantisWebSocketFrameHandler.java
@@ -75,11 +75,10 @@ public class MantisWebSocketFrameHandler extends SimpleChannelInboundHandler<Tex
                             queue.drainTo(items);
                         }
                         for (String data : items) {
-                            ctx.write(Unpooled.copiedBuffer(data, StandardCharsets.UTF_8));
+                            ctx.writeAndFlush(Unpooled.copiedBuffer(data, StandardCharsets.UTF_8));
                             numMessagesCounter.increment();
                             numBytesCounter.increment(data.length());
                         }
-                        ctx.flush();
                     }
                 } catch (Exception ex) {
                     log.error("Error writing to channel", ex);
